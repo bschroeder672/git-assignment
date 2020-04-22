3120b5cc1f17a9d1e4368ead92f0b3df596110f7
bschroeder672 <brendanschroeder2016@gmail.com>
2020-04-22 05:58:40 -0400

    added some code for getting answer and first if statement

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,6 +1,14 @@
 
 
+answer=$(ls | wc -l)
+
 echo "How many files are in the working directory?"
 echo "Type in a number and then press Enter:"
-read response
-echo "You guessed: $response"
+read guess
+echo "You guessed: $guess"
+
+
+if [[$guess -eq 4]]
+then
+  echo "Correct!"
+fi
