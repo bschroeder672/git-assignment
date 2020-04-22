890b428e208fd58560245944d2bb50ce42d6af7e
bschroeder672 <brendanschroeder2016@gmail.com>
2020-04-22 06:10:49 -0400

    created while loop

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,14 +1,24 @@
 
 
 answer=$(ls | wc -l)
+correct=false
 
 echo "How many files are in the working directory?"
 echo "Type in a number and then press Enter:"
 read guess
 echo "You guessed: $guess"
 
-
-if [[$guess -eq 4]]
-then
-  echo "Correct!"
-fi
+while [[$correct -eq 1]]
+do
+  if [[$guess -eq $answer]]
+  then
+    echo "Correct!"
+    correct=0
+  elif [[$guess -gt $answer]]
+  then
+    echo "Too high"
+  elif [[$guess -lt $answer]]
+  then
+    echo "Too low"
+  fi
+done
